b019ce46eb3ce52991197712b4510e182f6fe66c
Gbsam1 <faturoti3030@gmail.com>
2022-09-15 20:06:25 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_numbers: print the numbers, from 0 to 9
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num2 <= 9; num++)
+		_putcha(num + '0');
+	_putchar('\n');
